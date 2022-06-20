1ff4a537e73e0d5fd93c4b89814839bb99c13410
Texa-Dev <artemtroc26@gmail.com>
2022-06-20 19:08:40 +0300

    Init

---
+++ Human.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGitHW
+{
+    internal class Human
+    {
+        private string Name;
+
+        private int Age;
+
+        public Human(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string GetName() { return Name; }
+
+        public int GetAge() { return Age; } 
+    }
+}
