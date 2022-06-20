96094c3f4ef90e9f4ffa1ed41fefa43bd9062ac1
Texa-Dev <artemtroc26@gmail.com>
2022-06-20 19:21:06 +0300

    addClassWorker

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ProjectGitHW;
+
 Console.WriteLine("Hello, World!");
+
+Human Human = new Worker("Artem",34,15000);
+
---
+++ Worker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectGitHW
+{
+    internal class Worker : Human
+    {
+        private double Salary;
+
+        public Worker(string name, int age, double salary) : base(name, age)
+        {
+            Salary = salary;
+        }
+
+        public double GetSalary() { return Salary; }
+    }
+}
